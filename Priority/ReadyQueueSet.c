c9000fed5ead7c3f7f87716770a7da9b7685a2c9
kibeom <gkdnld99@gmail.com>
2024-05-17 01:44:40 +0900

    Priority edit

---
+++ Priority/ReadyQueueSet.c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+#include "SchedulingAlgorithm.h"
+
+// Queue 초기화 (Ready Queue에 있던 프로세스가 실행되는 과정)
+void Initialize(Process** process, int index) {
+    (*process)[index].processID = -1;
+    (*process)[index].arrivalTime = 0;
+    (*process)[index].burstTime = 0;
+    (*process)[index].priority = 0;
+}
